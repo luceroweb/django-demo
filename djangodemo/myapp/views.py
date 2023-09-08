a067686eacad53a8df210ffc2c1935b61fd4df13
Juan Lucero <luceroweb@gmail.com>
2023-09-08 10:19:20 -0700

    feat(templates): add base & home templates

--- djangodemo/myapp/views.py
+++ djangodemo/myapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
-def home(requests):
-    return HttpResponse("hello world!")
+def home(request):
+    return render(request, "home.html")
